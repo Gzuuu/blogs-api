bd9b5a010fd13f8e23e95a5bf7212fa2cf09fd05
Jesue Ferreira <jesue_gege@hotimail.com>
2023-05-10 17:05:05 -0300

    requisito 2 concluido

---
+++ src/models/User.js
@@ -0,0 +1,16 @@
+const UserModel = (sequelize, dataTypes) => {
+    const User = sequelize.define('User', {
+        id: { type: dataTypes.INTEGER, primaryKey : true, autoIncrement: true },
+        displayName: dataTypes.STRING,
+        email: dataTypes.STRING,
+        password: dataTypes.STRING,
+        image: dataTypes.STRING,
+    }, {
+        timestamps: false,
+        tableName: 'users',
+        underscored: true,
+    });
+    return User;
+};
+
+module.exports = UserModel;
